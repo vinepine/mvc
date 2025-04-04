ab1f3eaafb7a44131f8492f1140f399b761dbe99
Vinepine <vincentcalle@hotmail.com>
2025-04-04 19:52:40 +0200

    added route for api/quote

--- src/Controller/LuckyControllerJson.php
+++ src/Controller/LuckyControllerJson.php
@@ -21,4 +21,27 @@ class LuckyControllerJson
 
         return new JsonResponse($data);
     }
+
+    #[Route('/api/quote', name: 'quote')]
+    public function quote(): Response
+    {
+        $number = random_int(0, 2);
+        $quotes = ["One must be a wise reader to quote wisely and well.",
+        "To refuse awards is another way of accepting them with more noise than is normal.",
+        "Time is an illusion. Lunchtime doubly so."];
+        $date = date("Y-m-d");
+        $timestamp = time();
+        $data = [
+            'lucky-quote' => $quotes[$number],
+            'lucky-date' => $date,
+            'lucky-timestamp' => $timestamp
+        ];
+
+        $response = new JsonResponse($data);
+        $response->setEncodingOptions(
+            $response->getEncodingOptions() | JSON_PRETTY_PRINT
+        );
+        return $response;
+
+    }
 }
